e30352f09ab434f12e48deb9d53f268ff8b5b7c4
KezzaMcFezza <prokeero100@gmail.com>
2024-07-20 13:18:03 +0100

    gui added but electron is too big so i ignored it

--- serverlaunch.bat
+++ serverlaunch.bat
@@ -2,10 +2,8 @@
 echo Starting Octocon...
 cd /d "%~dp0"
 
-:: Start the server in a new window
 start "Octocon Server" cmd /k node server.js
 
-:: Start the GUI in a new window
 start "Octocon Monitor" cmd /k npm run monitor
 
 echo Octocon server and monitor have been launched.
