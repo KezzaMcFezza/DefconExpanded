ab1510179cd5e7490b39f9891078c9aa3b14f74d
KezzaMcFezza <prokeero100@gmail.com>
2025-06-06 20:24:59 +0100

    cleaned up some stuff in the html document with grammar, i also removed the debug logging in the discord api since it was flooding the logs. we only need to log if the discord api fails to recieve or send a request

--- node.js/apis/discord/discord.js
+++ node.js/apis/discord/discord.js
@@ -14,20 +14,14 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-
 const debug = require('../../debug-helpers');
 
 router.get('/api/discord-widget', async (req, res) => {
     const startTime = debug.enter('getDiscordWidget', [], 1);
-    debug.level2('Discord widget request');
     
     try {
-        debug.level3('Fetching Discord widget data from API');
         const discordResponse = await axios.get('https://discord.com/api/guilds/244276153517342720/widget.json');
-        debug.level3('Discord API response received:', { status: discordResponse.status });
         
-        debug.level2('Returning Discord widget data');
-        debug.exit('getDiscordWidget', startTime, 'success', 1);
         res.json(discordResponse.data);
     } catch (error) {
         debug.error('getDiscordWidget', error, 1);
