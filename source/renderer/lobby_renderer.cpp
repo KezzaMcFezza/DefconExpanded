9a73cbb676424053e7a8d1762252c993129d47b2
Keiron <prokeero100@gmail.com>
2025-10-03 00:55:13 +0100

    reduced mouse input latency on emscripten builds just by disabling MSAA on the emscripten builds reduced mouse latency for me and others this is crucial as the biggest complaint among users of late has been the mouse latency
    i assume this was because after making the eclipse ui system use buffers we actually
    increased the cpu overhead when we flush and rebind textures
    
    causing the mouse to be behind the queue of events

--- source/renderer/lobby_renderer.cpp
+++ source/renderer/lobby_renderer.cpp
@@ -470,9 +470,9 @@ void LobbyRenderer::RenderBorder()
 void LobbyRenderer::RenderVersionInfo()
 {
 #if defined(SYNC_PRACTICE)
-    char currentVersion[256] = "BETA" "  " "1.0";
+    char currentVersion[256] = "BETA" "  " "1.1";
 #elif defined(REPLAY_VIEWER_DESKTOP) || defined(TARGET_EMSCRIPTEN)
-    char currentVersion[256] = "RELEASE" "  " "1.13";
+    char currentVersion[256] = "RELEASE" "  " "1.16";
 #else
     char currentVersion[256] = APP_NAME "  " APP_VERSION;
 #endif
