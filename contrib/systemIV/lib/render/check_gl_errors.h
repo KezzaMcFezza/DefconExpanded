7e17c85f18ec293dfc42b6e8332152abd0e2aac3
Keiron <prokeero100@gmail.com>
2025-10-12 02:56:53 +0100

    completed the OpenGL error tracking API Added documentation at the top of the file so everyone knows how to use it, with examples the debug error checking is now complete and now offers more verbose output when used
    corrected the platform specific debug break operations
    added a new feature that tracks the error count per file if defined
    
    the formatting of the output has changed, we now truncate the file path from the output
    and only display the filename instead
    
    example:
    
    OpenGL Error in ClearScreen (renderer.cpp:578): GL_INVALID_ENUM (0x0500)
    
    instead of
    
    (C:\xampp\htdocs\contrib\systemIV\lib\render2d\renderer.cpp:578): GL_INVALID_ENUM (0x0500)
    
    ive used this API to help debug issues with a user and its provided excellent results
    this was inspired from the profiler in defcon, simple macro and a string

--- contrib/systemIV/lib/render/check_gl_errors.h
+++ contrib/systemIV/lib/render/check_gl_errors.h
@@ -6,6 +6,80 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/*
+
+  Very simple and effective OpenGL error checking API. The API usage is meant 
+  to be as simple as possible to prevent messy code.
+
+  USAGE:
+
+  Basic error checking and will provide good results
+    CHECK_GL_ERRORS("operation name");
+    Example: CHECK_GL_ERRORS("SetBlendMode");
+
+    
+  Debug error checking, more verbose and breaks on critical errors
+    CHECK_GL_ERRORS_DEBUG("operation name");
+    Example: CHECK_GL_ERRORS_DEBUG("DrawElements");
+
+
+  Break into debugger on any error:
+    CHECK_GL_ERRORS_BREAK("operation name");
+
+
+  For when you just want to check for errors without logging the operation name
+  More useful for performance, avoids string lookups
+    CHECK_GL_ERRORS_SILENT();
+
+
+  BUILD CONFIGURATION:
+  
+  To enable, define TRACK_GL_ERRORS:
+  #define TRACK_GL_ERRORS
+
+  And if you want to track the number of errors too:
+  #define TRACK_GL_ERROR_COUNT
+    
+  (CHECK_GL_ERRORS_DEBUG, CHECK_GL_ERRORS_BREAK) are only
+  available when _DEBUG is defined
+
+
+  OUTPUT FORMAT:
+  
+  Regular errors:
+    OpenGL Error in ClearScreen (renderer.cpp:699): GL_INVALID_ENUM (0x0500)
+
+  Silent errors:
+    OpenGL Error in unknown operation (renderer.cpp:789): GL_INVALID_ENUM (0x0500)
+    
+  Debug errors:
+    DEBUG OpenGL Error in DrawElements (renderer.cpp:1337): GL_INVALID_OPERATION (0x0502)
+    Warning: No shader program currently bound
+    Current VAO: 1
+    Current VBO: 2
+    Current FBO: 0
+
+*/
+
+//
+// platform specific debugger break
+
+#ifdef TARGET_MSVC
+    #define _DEBUG_BREAK() __debugbreak()
+#elif defined(TARGET_OS_LINUX) || defined(TARGET_OS_MACOSX)
+    #define _DEBUG_BREAK() __builtin_trap()
+#else
+    #define _DEBUG_BREAK() ((void)0)
+#endif
+
+#ifdef TRACK_GL_ERROR_COUNT
+static int g_glErrorCount = 0;
+#endif
+
+//
+// convert OpenGL error code to string
 
 static inline const char* glErrorToString(GLenum error) {
     switch (error) {
@@ -30,16 +104,51 @@ static inline const char* glErrorToString(GLenum error) {
     }
 }
 
+//
+// truncate filepath, just show filename
+
+static inline const char* getFilename(const char* filepath) {
+    if (!filepath) return "unknown file";
+    
+    const char* filename = filepath;
+    const char* lastSlash = strrchr(filepath, '/');
+    const char* lastBackslash = strrchr(filepath, '\\');
+    
+    if (lastSlash && lastSlash > filename) {
+        filename = lastSlash + 1;
+    }
+    if (lastBackslash && lastBackslash > filename) {
+        filename = lastBackslash + 1;
+    }
+    
+    return filename;
+}
+
+//
+// lightweight OpenGL error checking
+// can be used in release with minimal overhead
+
 static inline void checkGLErrors(const char* operation, const char* file, int line) {
     GLenum error = glGetError();
     if (error != GL_NO_ERROR) {
-        AppDebugOut("OpenGL Error in %s (%s:%d): %s (0x%04X)\n", 
+        #ifdef TRACK_GL_ERROR_COUNT
+        g_glErrorCount++;
+        AppDebugOut("OpenGL Error #%d in %s (%s:%d): %s (0x%04X)\n",
+                    g_glErrorCount,
                     operation ? operation : "unknown operation",
-                    file ? file : "unknown file",
+                    getFilename(file),
                     line,
                     glErrorToString(error), 
                     error);
-        
+        #else
+        AppDebugOut("OpenGL Error in %s (%s:%d): %s (0x%04X)\n", 
+                    operation ? operation : "unknown operation",
+                    getFilename(file),
+                    line,
+                    glErrorToString(error), 
+                    error);
+        #endif
+
         while ((error = glGetError()) != GL_NO_ERROR) {
             AppDebugOut("OpenGL Error: %s (0x%04X)\n", 
                         glErrorToString(error), 
@@ -48,30 +157,120 @@ static inline void checkGLErrors(const char* operation, const char* file, int li
     }
 }
 
+//
+// more verbose OpenGL error checking
+// more useful for integrated graphics, especially Intel Xe and HD graphics
+
+static inline void checkGLErrorsDebug(const char* operation, const char* file, int line) {
+    GLenum error = glGetError();
+    if (error != GL_NO_ERROR) {
+        GLenum firstError = error;
+        
+        #ifdef TRACK_GL_ERROR_COUNT
+        g_glErrorCount++;
+        AppDebugOut("DEBUG OpenGL Error #%d in %s (%s:%d): %s (0x%04X)\n",
+                    g_glErrorCount,
+                    operation ? operation : "unknown operation",
+                    getFilename(file),
+                    line,
+                    glErrorToString(error), 
+                    error);
+        #else
+        AppDebugOut("DEBUG OpenGL Error in %s (%s:%d): %s (0x%04X)\n", 
+                    operation ? operation : "unknown operation",
+                    getFilename(file),
+                    line,
+                    glErrorToString(error), 
+                    error);
+        #endif
+
+        GLint currentProgram;
+        glGetIntegerv(GL_CURRENT_PROGRAM, &currentProgram);
+        if (currentProgram == 0) {
+            AppDebugOut("Warning: No shader program currently bound\n");
+        } else {
+            AppDebugOut("Current Program: %d\n", currentProgram);
+        }
+        
+        //
+        // check VAO binding
+
+        GLint currentVAO;
+        glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &currentVAO);
+        AppDebugOut("  Current VAO: %d\n", currentVAO);
+        
+        //
+        // check VBO binding
+
+        GLint currentVBO;
+        glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &currentVBO);
+        AppDebugOut("  Current VBO: %d\n", currentVBO);
+        
+        //
+        // check framebuffer binding
+        
+        GLint currentFBO;
+        glGetIntegerv(GL_FRAMEBUFFER_BINDING, &currentFBO);
+        AppDebugOut("  Current FBO: %d\n", currentFBO);
+        
+        if (currentFBO != 0) {
+            GLenum fbStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+            if (fbStatus != GL_FRAMEBUFFER_COMPLETE) {
+                AppDebugOut("Warning: Framebuffer is incomplete (0x%04X)\n", fbStatus);
+            }
+        }
+        
+        while ((error = glGetError()) != GL_NO_ERROR) {
+            AppDebugOut("DEBUG OpenGL Error: %s (0x%04X)\n", 
+                        glErrorToString(error), 
+                        error);
+        }
+        
+        //
+        // break on critical errors in debug builds
+
+        #ifdef _DEBUG
+        if (firstError == GL_OUT_OF_MEMORY || 
+            firstError == GL_INVALID_OPERATION ||
+            firstError == GL_INVALID_FRAMEBUFFER_OPERATION) {
+            _DEBUG_BREAK();
+        }
+        #endif
+        
+        (void)firstError;
+    }
+}
+
+//
+// return nothing if error tracking is disabled
+
 #ifndef TRACK_GL_ERRORS
-#define CHECK_GL_ERRORS(operation) ((void)0)
-#define CHECK_GL_ERRORS_DEBUG(operation) ((void)0)
-#define CHECK_GL_ERRORS_SILENT() ((void)0)
-#define CHECK_GL_ERRORS_BREAK(operation) ((void)0)
+    #define CHECK_GL_ERRORS(operation) ((void)0)
+    #define CHECK_GL_ERRORS_DEBUG(operation) ((void)0)
+    #define CHECK_GL_ERRORS_SILENT() ((void)0)
+    #define CHECK_GL_ERRORS_BREAK(operation) ((void)0)
 #endif
+
+//
+// define macros when tracking is enabled
 
 #ifdef TRACK_GL_ERRORS
-#define CHECK_GL_ERRORS(operation) checkGLErrors(operation, __FILE__, __LINE__)
-#define CHECK_GL_ERRORS_SILENT() checkGLErrors(NULL, __FILE__, __LINE__)
+    #define CHECK_GL_ERRORS(operation) checkGLErrors(operation, __FILE__, __LINE__)
+    #define CHECK_GL_ERRORS_SILENT() checkGLErrors(NULL, __FILE__, __LINE__)
 
-#ifdef _DEBUG
-    #define CHECK_GL_ERRORS_DEBUG(operation) checkGLErrors(operation, __FILE__, __LINE__)
+    #ifdef _DEBUG
+        #define CHECK_GL_ERRORS_DEBUG(operation) checkGLErrorsDebug(operation, __FILE__, __LINE__)
+        
+        #define CHECK_GL_ERRORS_BREAK(operation) do { \
+            GLenum error = glGetError(); \
+            if (error != GL_NO_ERROR) { \
+                checkGLErrors(operation, __FILE__, __LINE__); \
+                _DEBUG_BREAK(); \
+            } \
+        } while(0)
+    #else
+        #define CHECK_GL_ERRORS_DEBUG(operation) ((void)0)
+        #define CHECK_GL_ERRORS_BREAK(operation) ((void)0)
+    #endif
 #endif
-
-#ifdef _DEBUG
-    #define CHECK_GL_ERRORS_BREAK(operation) do { \
-        GLenum error = glGetError(); \
-        if (error != GL_NO_ERROR) { \
-            checkGLErrors(operation, __FILE__, __LINE__); \
-            __debugbreak(); \
-        } \
-    } while(0)
-#endif
-#endif
-
 #endif
