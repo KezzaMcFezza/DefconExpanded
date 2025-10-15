83792aedf135c656db553ef628fff72c992d2b50
Keiron <prokeero100@gmail.com>
2025-10-15 05:05:48 +0100

    updated sync practice and replay viewer version we also do not include the mods directory in the preloaded data folder for emscripten for sync practice builds to reduce the size. sync practice builds are now 2mb in size

--- source/renderer/lobby_renderer.cpp
+++ source/renderer/lobby_renderer.cpp
@@ -470,9 +470,9 @@ void LobbyRenderer::RenderBorder()
 void LobbyRenderer::RenderVersionInfo()
 {
 #if defined(SYNC_PRACTICE)
-    char currentVersion[256] = "BETA" "  " "1.15";
+    char currentVersion[256] = "BETA" "  " "1.16";
 #elif defined(REPLAY_VIEWER) || defined(REPLAY_VIEWER_DESKTOP)
-    char currentVersion[256] = "RELEASE" "  " "1.20.4";
+    char currentVersion[256] = "RELEASE" "  " "1.22";
 #else
     char currentVersion[256] = APP_NAME "  " APP_VERSION;
 #endif
