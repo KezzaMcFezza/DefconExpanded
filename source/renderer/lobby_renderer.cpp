53f191d82059cd02414f5960be1446bd67b86504
Keiron <prokeero100@gmail.com>
2026-01-21 16:40:12 +0000

    Updated replay viewer and sync practice client version strings, added a space to the FPS counter

--- source/renderer/lobby_renderer.cpp
+++ source/renderer/lobby_renderer.cpp
@@ -467,7 +467,7 @@ void LobbyRenderer::RenderBorder()
 void LobbyRenderer::RenderVersionInfo()
 {
 #if defined(SYNC_PRACTICE)
-    char currentVersion[256] = "BETA" "  " "1.16.5";
+    char currentVersion[256] = "BETA" "  " "1.16.9";
 #elif defined(REPLAY_VIEWER) || defined(REPLAY_VIEWER_DESKTOP)
     char currentVersion[256] = "RELEASE" "  " APP_BUILD_NUMBER;
 #else
