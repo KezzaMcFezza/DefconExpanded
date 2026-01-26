f8e8a580e6684c8639078c5f86745662c40b63c4
Keiron <prokeero100@gmail.com>
2026-01-26 12:13:35 +0000

    Remove Broken MacOS display handling?

--- contrib/systemIV/lib/gucci/macosxlibrary.cpp
+++ contrib/systemIV/lib/gucci/macosxlibrary.cpp
@@ -2,14 +2,6 @@
 #include <CoreGraphics/CoreGraphics.h>
 
 
-void GetMainDisplayResolution( int &width, int &height )
-{
-	CGRect rect = CGDisplayBounds( CGMainDisplayID() );
-	width = (int)rect.size.width;
-	height = (int)rect.size.height;
-}
-
-
 void MacOSXHideWin()
 {
 	ProcessSerialNumber me;
